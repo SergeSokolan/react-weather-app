f8353242a081fcced82a25bc7d0a0fcda89caaad
SergeS <sokolan.sergey@gmail.com>
2017-09-12 22:13:19 +0300

    that's all

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -19,9 +19,9 @@ class WeatherDisplay extends Component {
   }
   componentDidMount() {
     const zip = this.props.zip;
-    // const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
-    //   zip +
-    //   "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
+    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+      zip +
+       "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
      fetch(URL).then(res => res.json()).then(json => {
       this.setState({ weatherData: json });
     });
@@ -30,7 +30,7 @@ class WeatherDisplay extends Component {
     const weatherData = this.state.weatherData;
     if (!weatherData) return <div>Loading</div>;
     const weather = weatherData.weather[0];
-   // const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
     return (
       <div>
         <h1>
